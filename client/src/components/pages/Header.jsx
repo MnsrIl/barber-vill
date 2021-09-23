bc49f16e446acbff0e6e3358be6c0ceb9b410ea8
Seda805 <dudaeva44@gmail.com>
2021-09-23 12:29:07 +0300

    маленькие изменения,связанные с кнопками

--- client/src/components/pages/Header.jsx
+++ client/src/components/pages/Header.jsx
@@ -3,29 +3,40 @@ import { Box } from "@mui/system";
 import React from "react";
 import profileIcon from "../../image/icons-login.png";
 import GitHubIcon from "@material-ui/icons/GitHub";
+import { useHistory } from "react-router";
 
 const useStyles = makeStyles((theme) => ({
   iconProfile: {
     width: 45,
     height: 45,
   },
+  headerMain: {
+    display: "flex",
+    height: 52,
+    margin: "30px 150px",
+    alignItems: "center",
+  },
+  headerInfo: {
+    display: "flex",
+    flex: "1",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
 }));
 
 function Header() {
   const classes = useStyles();
+  const history = useHistory();
   return (
     <Grid xs={12}>
-      <Box display="flex" height={52} mx="150px" mt="58px" alignItems="center">
+      <Box className={classes.headerMain}>
         <Grid item xs={12} sm={3}>
           logo
         </Grid>
-        <Box
-          display="flex"
-          flex="1"
-          justifyContent="space-between"
-          alignItems="center"
-        >
-          <Box>Главная </Box>
+        <Box className={classes.headerInfo}>
+          <Box style={{ cursor: "pointer" }} onClick={() => history.push("/")}>
+            Главная
+          </Box>
           <Box>Парикмахеры </Box>
           <Box>О сервисе </Box>
           <Box>
