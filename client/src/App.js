14c37b02090384dac8a2154b79820a4bd98852d2
TheZiggie <mansur-2004-09@mail.ru>
2021-09-25 18:00:10 +0300

    Вынес логику с загрузкой данных авторизованного пользователя в компонент с роутами

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,7 @@
-import {useDispatch, useSelector} from "react-redux";
-import {useEffect} from "react";
-import {loadUser} from "./redux/feautures/auth";
 import Routes from "./routes";
 
 
 function App() {
-    const isLoggedIn = useSelector(store => store.auth.isLoggedIn);
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        isLoggedIn && dispatch(loadUser())
-    }, [isLoggedIn]);
 
     return (
         <Routes />
--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,11 +1,18 @@
 import {Redirect, Route, Switch} from "react-router-dom";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import allRoutesList from "./forAll";
 import unauthorizedList from "./forUnauthorized";
 import authorizedList from "./forAuthorized";
+import {useEffect} from "react";
+import {loadUser} from "../redux/feautures/auth";
 
 const Routes = () => {
     const isLoggedIn = useSelector(store => store.auth.isLoggedIn);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        isLoggedIn && dispatch(loadUser())
+    }, [isLoggedIn]);
 
     const authList = isLoggedIn ? authorizedList : unauthorizedList;
     const routesList = [...allRoutesList, ...authList];
