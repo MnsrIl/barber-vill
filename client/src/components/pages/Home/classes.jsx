21812d6ea6e72ce21cdc48a80453be831b246d07
Seda805 <dudaeva44@gmail.com>
2021-09-27 13:47:16 +0300

    исправила ошибку с анимацией

--- client/src/components/pages/Home/classes.jsx
+++ client/src/components/pages/Home/classes.jsx
@@ -2,126 +2,122 @@ import { makeStyles } from "@material-ui/core/styles";
 import barberShop from "../../../image/barbershop1.jpg";
 
 const useStyles = makeStyles((theme) => ({
-    main: {
-      position: "relative",
-      backgroundImage: `url(${barberShop})`,
-      backgroundRepeat: "no-repeat",
-      backgroundSize: "100% 900px",
-      height: 720,
+  main: {
+    position: "relative",
+    backgroundImage: `url(${barberShop})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "100% 900px",
+    height: 720,
+  },
+  card: {
+    width: 250,
+    marginLeft: 50,
+    borderRadius: 15,
+    position: "relative",
+    overflow: "hidden",
+    "&:hover img": {
+      transform: "scale(1.2)",
     },
-    card: {
-      width: 250,
-      marginLeft: 50,
-      borderRadius: 15,
-      position: "relative",
-      overflow: "hidden",
-    },
-    btn: {
-      margin: 20,
-      borderRadius: 40,
-      fontSize: 12,
-      padding: 12,
-    },
-    iconProfile: {
-      width: 50,
-      height: 50,
-    },
-    content: {
-      display: "flex",
-      justifyContent: "space-between",
-    },
-    contenText:{
-      width:340, 
-      height:306, 
-      marginLeft:"150px", 
-      marginTop:"80px", 
-      color:"white",
-      fontSize:18
-      
-    },
-    triangle: {
-      width: 250,
-      background: "linear-gradient(297deg, transparent 1.5em, #e9f666 0 )",
-      opacity:0,
-      transition: "1s",
-      animation: `$show 1000ms ${theme.transitions.easing.easeInOut}`,
-      animationFillMode: "forwards",
-      animationDelay: "1s",
-      
-    },
-    triangleB: {
-      width: 250,
-      background: "linear-gradient(116deg, transparent 1.5em, #2685b9 0)",
-      opacity:0,
-      transition: "1s",
-      animation: `$show 4000ms ${theme.transitions.easing.easeInOut}`,
-      animationFillMode: "forwards",
-      animationDelay: "1s",
-    },
-    "@keyframes show": {
-      "0%": { opacity: 0 },
-      "100%": { opacity: 1 },
-    },
-    cardIcon: {
-      position: "absolute",
-      fontSize: 30,
-      color: "black",
-      right: 0,
-      left: 0,
-      top: 0,
-      bottom: 0,
-      width: 60,
-      height: 60,
-      margin: "auto",
-      textAlign: "center",
-      lineHeight: 60,
-      transform: "scale(3)",
-      background: "rgba(255,255,255, 0.3)",
-      boxShadow: "0 0 0 8px rgba(180,180,180, 0.3)",
-      opacity: 0,
+    "&:hover svg": {
+      opacity: 1,
+      transform: "scale(1)",
       transition: "all 0.5s",
-      zIndex: 2,
-      "&:hover": {
-        opacity: 1,
-        transform: "scale(1)",
-      },
     },
-    cardImg: {
-      width: "100%",
+  },
+  cardIcon: {
+    position: "absolute",
+    color: "red",
+    right: 0,
+    left: 0,
+    top: 0,
+    bottom: 0,
+    margin: "auto",
+    textAlign: "center",
+    lineHeight: 60,
+    transform: "scale(3)",
+    background: "rgba(255,255,255, 0.3)",
+    boxShadow: "0 0 0 15px rgba(180,180,180, 0.3)",
+    borderRadius: "100%",
+    opacity: 0,
+    zIndex: 2,
+  },
+  cardImg: {
+    width: "100%",
+    height: "100%",
+    transition: "all 0.5s",
+  },
+  cardLink: {
+    "&::before": {
+      position: "absolute",
+      content: '""',
+      background: "rgba(0,0,0, 0.8)",
+      width: 0,
       height: "100%",
+      left: "50%",
       transition: "all 0.5s",
-      "&:hover": {
-        transform: "scale(1.2)",
-      },
+      zIndex: 1,
     },
-    cardLink: {
+    "&:hover": {
       "&::before": {
-        position: "absolute",
-        content: '""',
-        background: "rgba(0,0,0, 0.4)",
-        width: 0,
-        height: "100%",
-        left: "50%",
-        transition: "all 0.5s",
-        zIndex: 1,
-      },
-      "&:hover": {
-        "&::before": {
-          width: "100%",
-          left: 0,
-        },
+        width: "100%",
+        left: 0,
       },
     },
-    footer: {
-      width: "90%",
-      height: "60px",
-      position: "absolute",
-      bottom: "0",
-      borderTop: "1px solid grey",
-      margin: "0 50px",
-      textAlign: "center",
-      paddingTop: "10px",
-    },
-  }));
+  },
+  footer: {
+    width: "90%",
+    height: "60px",
+    position: "absolute",
+    bottom: "0",
+    borderTop: "1px solid grey",
+    margin: "0 50px",
+    textAlign: "center",
+    paddingTop: "10px",
+  },
+  btn: {
+    margin: 20,
+    borderRadius: 40,
+    fontSize: 12,
+    padding: 12,
+  },
+  iconProfile: {
+    width: 50,
+    height: 50,
+  },
+  content: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  contenText: {
+    width: 340,
+    height: 306,
+    marginLeft: "150px",
+    marginTop: "80px",
+    color: "white",
+    fontSize: 18,
+  },
+  triangle: {
+    width: 250,
+    background: "linear-gradient(297deg, transparent 1.5em, #e9f666 0 )",
+    opacity: 0,
+    transition: "1s",
+    animation: `$show 1000ms ${theme.transitions.easing.easeInOut}`,
+    animationFillMode: "forwards",
+    animationDelay: "1s",
+  },
+  triangleB: {
+    width: 250,
+    background: "linear-gradient(116deg, transparent 1.5em, #2685b9 0)",
+    opacity: 0,
+    transition: "1s",
+    animation: `$show 4000ms ${theme.transitions.easing.easeInOut}`,
+    animationFillMode: "forwards",
+    animationDelay: "1s",
+  },
+  "@keyframes show": {
+    "0%": { opacity: 0 },
+    "100%": { opacity: 1 },
+  },
+}));
 
-  export default useStyles
+export default useStyles;
--- client/src/components/pages/barbers/AllBarbersPage.js
+++ client/src/components/pages/barbers/AllBarbersPage.js
@@ -9,14 +9,72 @@ import {
 import { Box } from "@mui/system";
 import React from "react";
 import Header from "../Header";
-import "./index.css";
 
 const useStyles = makeStyles((theme) => ({
+  main: {
+    margin: 0,
+    height: "120%",
+  },
+  header: {
+    position: "relative",
+    overflow: "hidden",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "flex-start",
+    alignContent: "flex-start",
+    height: "50vw",
+    minHeight: 400,
+    maxHeight: 550,
+    minWidth: 300,
+    color: "#eee",
+    width: "100%",
+    "&:after": {
+      content: "''",
+      width: "100%",
+      height: "100%",
+      position: "absolute",
+      bottom: 0,
+      left: 0,
+      zIndex: -1,
+      background:
+        "linear-gradient(to bottom, rgba(0,0,0,0.12) 40%,rgba(27,32,48,1) 100%)",
+    },
+    "&:before": {
+      content: "''",
+      width: "100%",
+      height: "200%",
+      position: "absolute",
+      top: 0,
+      left: 0,
+      webkitBackfaceVisibility: "hidden",
+      webkitTransform: "translateZ(0)",
+      backfaceVisibility: "hidden",
+      transform: "translateZ(0)",
+      background:
+        "#1B2030 url('https://top-barbershop.com/media/2017/05/Barber2vk-1800x1194.jpg') 50% 0 no-repeat",
+      backgroundSize: "100%",
+      backgroundAttachment: "fixed",
+      animation: `$grow 360s  linear 10ms infinite`,
+      transition: "all 0.4s ease-in-out",
+      zIndex: -2,
+    },
+  },
+  "@keyframes grow": {
+    "0%": {
+      transform: "scale(1) translateY(0px)",
+    },
+    "50%": {
+      transform: "scale(1.2) translateY(-400px)",
+    },
+  },
   cardBox: {
     position: "relative",
     overflow: "hidden",
-    marginRight: 25, 
-    marginBottom: 25
+    marginRight: 25,
+    marginBottom: 25,
+    "&:hover img": {
+      transform: "scale(1.2)",
+    },
   },
   card: {
     display: "block",
@@ -48,7 +106,6 @@ const useStyles = makeStyles((theme) => ({
     height: "120px",
     margin: "auto",
     textAlign: "center",
-    // lineHeight: 60,
     transform: "scale(5)",
     boxShadow: "0 0 0 2px rgba(180,180,180, 0.7)",
     borderRadius: "10%",
@@ -62,9 +119,6 @@ const useStyles = makeStyles((theme) => ({
   },
   img: {
     transition: "all 0.5s",
-    "&:hover": {
-      transform: "scale(1.2)",
-    },
   },
 }));
 
@@ -72,43 +126,18 @@ function AllBarbersPage() {
   const classes = useStyles();
 
   return (
-    <Grid container class="main">
-      <Grid class="header">
+    <Grid container className={classes.main}>
+      <Grid className={classes.header}>
         <Header />
       </Grid>
 
       <Grid item xs={12} style={{ margin: 100, textAlign: "center" }}>
-        <Typography variant="h2" style={{color:'white', }}>Все парикмахеры</Typography>
+        <Typography variant="h2" style={{ color: "white" }}>
+          Все парикмахеры
+        </Typography>
       </Grid>
 
-      <Grid container style={{ display: "flex", justifyContent: "center"}}>
-        <Grid item xs={12} sm={3} className={classes.cardBox}>
-          <Card className={classes.card}>
-            <Box>
-              <CardMedia
-                component={"img"}
-                src={
-                  "https://oldboybarbershop.com/sites/default/files/2019-10/krasnodar-sedina-barber-denis.jpg"
-                }
-                className={classes.img}
-              />
-              <Box className={classes.hiddenText}>
-                <Typography>lorem</Typography>
-                <Typography>ipsum</Typography>
-              </Box>
-              <CardContent>
-                <Typography
-                  variant="h5"
-                  component="h2"
-                  style={{ textAlign: "center" }}
-                >
-                  Lorem ipsum
-                </Typography>
-              </CardContent>
-            </Box>
-          </Card>
-        </Grid>
-        
+      <Grid container style={{ display: "flex", justifyContent: "center" }}>
         <Grid item xs={12} sm={3} className={classes.cardBox}>
           <Card className={classes.card}>
             <Box>
@@ -163,6 +192,32 @@ function AllBarbersPage() {
           </Card>
         </Grid>
 
+        <Grid item xs={12} sm={3} className={classes.cardBox}>
+          <Card className={classes.card}>
+            <Box>
+              <CardMedia
+                component={"img"}
+                src={
+                  "https://oldboybarbershop.com/sites/default/files/2019-10/krasnodar-sedina-barber-denis.jpg"
+                }
+                className={classes.img}
+              />
+              <Box className={classes.hiddenText}>
+                <Typography>lorem</Typography>
+                <Typography>ipsum</Typography>
+              </Box>
+              <CardContent>
+                <Typography
+                  variant="h5"
+                  component="h2"
+                  style={{ textAlign: "center" }}
+                >
+                  Lorem ipsum
+                </Typography>
+              </CardContent>
+            </Box>
+          </Card>
+        </Grid>
       </Grid>
     </Grid>
   );
